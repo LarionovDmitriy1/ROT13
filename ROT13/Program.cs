bac2193c1f6da81980cbe324f735bf38af1e9185
Дима Ларионов <dima_larionov_000@mail.ru>
2022-12-08 23:50:20 +0300

    Добавьте файлы проекта.

---
+++ ROT13/Program.cs
@@ -0,0 +1,9 @@
+﻿using ROT13;
+while (true)
+{
+    Console.WriteLine();
+    Console.WriteLine("Введите текст");
+    Console.WriteLine();
+    string enter = Console.ReadLine();
+    ParseInWork13.WordEncryption(enter);
+}
